1881d655057657f916f0386d478bd861bdf9c44e
KavyaVerma3223564 <Kavya_Verma1@hotmail.com>
2026-05-07 21:27:04 +0530

    New

--- Defult.exe.bat
+++ Defult.exe.bat
@@ -28,6 +28,9 @@ curl -L -o C:\AlienAutomation\vscode.exe https://update.code.visualstudio.com/la
 echo Installing VS Code...
 C:\AlienAutomation\vscode.exe /silent
 
+echo Applying Aliens Theme...
+mkdir "%APPDATA%\Code - Insiders\User"
+echo { "workbench.colorTheme": "Dark ( Aliens Theme)" } > "%APPDATA%\Code - Insiders\User\settings.json"
 
 echo Enabling Dark Theme...
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v AppsUseLightTheme /t REG_DWORD /d 0 /f
