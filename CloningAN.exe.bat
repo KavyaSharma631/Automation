849b51c693ea4e7dba7fed77de54a63728f5e09e
KavyaVerma3223564 <Kavya_Verma1@hotmail.com>
2026-05-07 16:02:35 +0530

    Add files via upload

---
+++ CloningAN.exe.bat
@@ -0,0 +1,40 @@
+@ECHO OFF
+REM BFCPEOPTIONSTART
+REM Advanced BAT to EXE Converter www.BatToExeConverter.com
+REM BFCPEEXE=
+REM BFCPEICON=
+REM BFCPEICONINDEX=-1
+REM BFCPEEMBEDDISPLAY=0
+REM BFCPEEMBEDDELETE=1
+REM BFCPEADMINEXE=0
+REM BFCPEINVISEXE=0
+REM BFCPEVERINCLUDE=0
+REM BFCPEVERVERSION=1.0.0.0
+REM BFCPEVERPRODUCT=Product Name
+REM BFCPEVERDESC=Product Description
+REM BFCPEVERCOMPANY=Your Company
+REM BFCPEVERCOPYRIGHT=Copyright Info
+REM BFCPEWINDOWCENTER=1
+REM BFCPEDISABLEQE=0
+REM BFCPEWINDOWHEIGHT=30
+REM BFCPEWINDOWWIDTH=120
+REM BFCPEWTITLE=Window Title
+REM BFCPEOPTIONEND
+@echo off
+cd C:/Aliens && git clone -b main git@github.com:Aliens-Company/.github.git
+cd C:/Aliens && git clone -b main git@github.com:Aliens-Company/Cyborg.git
+
+cd C:/Aliens && git clone -b main git@github.com:Aliens-Company/Copilot.git
+cd C:/Aliens && git clone -b main git@github.com:Aliens-Company/Project.git
+cd C:/Aliens && git clone -b main git@github.com:Aliens-Company/Docs.git
+cd C:/Aliens && git clone -b main git@github.com:Aliens-Company/Blog.git
+cd C:/Aliens && git clone -b main git@github.com:Aliens-Company/Report.git
+cd C:/Aliens && git clone -b main git@github.com:Aliens-Company/Attendance.git
+cd C:/Aliens && git clone -b main git@github.com:Aliens-Company/Secret.git
+cd C:/Aliens && git clone -b main git@github.com:Aliens-Company/Tools.git
+
+cd C:/Aliens && git clone -b main git@github.com:Aliens-Company/.Alien.git
+cd C:/Aliens && git clone -b main git@github.com:Aliens-Company/WebOS.git
+cd C:/Aliens && git clone -b main git@github.com:Aliens-Company/WebApp.git
+cd C:/Aliens && git clone -b main git@github.com:Aliens-Company/WebSDK.git
+pause
