fa6497b7312ad2620b2ea0b09d1c5d528c7ad5f2
KavyaVerma3223564 <Kavya_Verma1@hotmail.com>
2026-05-07 21:51:42 +0530

    New

--- Defult.exe.bat
+++ Defult.exe.bat
@@ -43,20 +43,11 @@ echo.
 set /p NEWPCNAME=Enter New PC Name: 
 wmic computersystem where name="%computername%" call rename name="%NEWPCNAME%"
 
-@echo off
-
+echo.
 set /p USERNAME=Enter Username: 
-set /p OLDPASS=Enter Old Password: 
 set /p NEWPASS=Enter New Password: 
 
-net use \\localhost /user:%USERNAME% %OLDPASS% >nul 2>&1
+net user "%USERNAME%" "%NEWPASS%"
 
-if %errorlevel%==0 (
-    net user %USERNAME% %NEWPASS%
-    echo Password Changed Successfully
-) else (
-    echo Old Password Wrong
-)
-
+echo Password Changed Successfully
 pause
-
