effd890f4fc5aea3ec0b70050938891505da3c5f
KavyaVerma3223564 <Kavya_Verma1@hotmail.com>
2026-05-09 12:47:54 +0530

    New

--- Defult.exe.bat
+++ Defult.exe.bat
@@ -24,6 +24,8 @@ REM BFCPEOPTIONEND
 
 @echo off
 
+mkdir C:\Automation
+
 echo Enabling Dark Theme...
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v AppsUseLightTheme /t REG_DWORD /d 0 /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v SystemUsesLightTheme /t REG_DWORD /d 0 /f
@@ -33,9 +35,7 @@ reg add "HKCU\Control Panel\Colors" /v Background /t REG_SZ /d "0 0 0" /f
 reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "" /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Wallpapers" /v BackgroundType /t REG_DWORD /d 1 /f
 
-RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
-taskkill /f /im explorer.exe
-start explorer.exe
+powershell -command "Add-Type -TypeDefinition 'using System;using System.Runtime.InteropServices;public class W{[DllImport(\"user32.dll\")]public static extern bool SystemParametersInfo(int uAction,int uParam,string lpvParam,int fuWinIni);}'; [W]::SystemParametersInfo(20,0,'',3)"
 
 echo.
 set /p NEWPCNAME=Enter New PC Name:
@@ -64,7 +64,6 @@ call "%LocalAppData%\Programs\Microsoft VS Code Insiders\bin\code-insiders.cmd" 
 timeout /t 10 /nobreak
 
 echo Applying Aliens Theme...
-
 mkdir "%APPDATA%\Code - Insiders\User"
 
 (
